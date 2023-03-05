b5001792186e476dc018686f6e5c4fca77c46399
BNEEE <bneeeee0@gmail.com>
2023-03-05 20:06:53 +0200

    update profile 2

--- controllers/admin/auth.js
+++ controllers/admin/auth.js
@@ -35,14 +35,12 @@ export const profile =async (req, res, next) => {
       }
       res.render('admin/auth/profile', { 
         layout: './admin/layouts/main', 
-        owner: req.session.owner 
+        owner: req.session.owner
       });
   } catch(err){
       next(err)
   }
 }
-
-
 
 export const register = async (req,res,next)=>{
     try {
@@ -142,20 +140,6 @@ export const updateOwner =async (req,res,next)=>{
   try {
       // TODO:: add update image when there is an image has been selected
       const updatedOwner = await Owner.findByIdAndUpdate(req.session.owner._id,{$set: req.body},{new:true})
-      const ownerResponse = {
-        _id: updatedOwner._id,
-        ownername: updatedOwner.ownername,
-        companyname: updatedOwner.companyname,
-        email: updatedOwner.email,
-        phone: updatedOwner.phone,
-        image: updatedOwner.image,
-        isAdmin: updatedOwner.isAdmin,
-        access_token: req.session.owner.access_token
-      };
-      
-      req.session.owner = ownerResponse;
-
-
       res.redirect('/admin/auth/profile');
   }
   catch(err){
